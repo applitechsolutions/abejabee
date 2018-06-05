b89f14064804dd372773205d63413e2da59b831d
LnkColumbus <marlonveliz.nero19@gmail.com>
2018-06-04 18:31:41 -0600

    KK

--- html/index/index.php
+++ html/index/index.php
@@ -876,6 +876,21 @@
         <!-- /#page-wrapper -->
     </div>
     <script src="UI/jquery.autocomplete.js"></script>
+    <script src="UI/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="UI/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="UI/js/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="UI/js/raphael.min.js"></script>
+    <script src="UI/js/morris.min.js"></script>
+    <script src="UI/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="UI/js/sb.js"></script>
     
 </body>
 </html>
