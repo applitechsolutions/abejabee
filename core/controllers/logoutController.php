20e3aa776712b5bad17bebc5f0382348d786c85d
LuisPedroChaves <luispedrochaves@hotmail.com>
2018-06-07 19:48:40 -0600

    logout.MARLONZORRA

--- core/controllers/logoutController.php
+++ core/controllers/logoutController.php
@@ -7,7 +7,6 @@
       $_SESSION['app_id'] == NULL ;
       unset($_SESSION['app_id']);
       session_destroy();                  
-      //redireccionamos al index
       include(HTML_DIR.'public/login.php');
    }
 ?>
