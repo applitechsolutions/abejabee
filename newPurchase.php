859edfc83260ccd658ef345369b85d8a3729e4d9
LuisPedroChaves <luispedrochaves@hotmail.com>
2018-07-20 17:16:44 -0600

    Purchase 1.0

--- newPurchase.php
+++ newPurchase.php
@@ -58,94 +58,112 @@ include_once 'funciones/bd_conexion.php';
           </div>
 
           <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
               <form role="form" id="form-purchase" name="form-purchase" method="post" action="BLL/purchase.php">
                 <div class="box-body">
-                  <div class="form-group">
-                    <label>Fecha</label>
-                    <div class="input-group date">
-                      <div class="input-group-addon">
-                        <i class="fa fa-calendar"></i>
-                      </div>
-                      <input type="text" class="form-control pull-right datepicker" id="datepicker" name="date">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <span class="text-danger text-uppercase">*</span>
-                    <label for="codigo">Código</label>
-                    <input type="text" class="form-control" id="code" name="code" placeholder="Escriba un código">
-                  </div>
-                  <div class="form-group">
-                    <span class="text-danger text-uppercase">*</span>
-                    <label for="costo">Costo</label>
-                    <div class="input-group">
-                      <span class="input-group-addon">
-                        <i class="fa fa-money"></i>
-                      </span>
-                      <input type="number" id="cost" name="cost" placeholder="0.00" min="0.00" step="0.01" class="form-control">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="descripcion">Descripción</label>
-                    <textarea class="form-control" rows="3" id="description" name="description" placeholder="Escriba la descripción del producto... "></textarea>
-                  </div>
-                </div>
-            </div>
-            <!-- /.form-group -->
-            <!-- /.col -->
-            <div class="col-md-6">
-              <div class="box-body">
-                <div class="form-group">
-                  <label for="picture">Imagen</label>
-                  <input type="file" id="picture" name="file">
+                  <div class="row">
 
-                  <p class="help-block">Ingrese la imagen del producto aquí.</p>
-                </div>
-                <div class="form-group">
-                  <span class="text-danger text-uppercase">*</span>
-                  <label>Marca</label>
-                  <button type="button" class="btn btn-Normal bg-teal-active btn-xs pull-right" data-toggle="modal" data-target="#modal-make">+ Crear Nueva</button>
-                  <select id="make" name="make" class="form-control select2" style="width: 100%;" value="0">
-                    <option value="" selected>Seleccione una marca</option>
-                    <?php
-                      try {
-                        $sql = "SELECT * FROM make";
-                        $resultado = $conn->query($sql);
-                        while ($make_product = $resultado->fetch_assoc()) {?>
-                      <option value="<?php echo $make_product['idMake']; ?>">
-                        <?php echo $make_product['makeName']; ?>
-                      </option>
-                      <?php 
-                        }
-                      } catch (Exception $e) {
-                          echo "Error: " . $e->getMessage();
-                      }
-                    ?>
-                  </select>
-                </div>
+                    <div class="col-lg-3">
+                      <div class="form-group">
+                        <span class="text-danger text-uppercase">*</span>
+                        <label>Fecha</label>
+                        <div class="input-group date">
+                          <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                          </div>
+                          <input type="text" class="form-control pull-right datepicker" id="datepicker" name="date">
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <span class="text-danger text-uppercase">*</span>
+                        <label>Proveedor</label>
+                        <select id="provider" name="provider" class="form-control select2" style="width: 100%;" value="0">
+                          <option value="" selected>Seleccione un proveedor</option>
+                          <?php
+                                try {
+                                  $sql = "SELECT * FROM provider";
+                                  $resultado = $conn->query($sql);
+                                  while ($pro_purchase = $resultado->fetch_assoc()) {?>
+                            <option value="<?php echo $pro_purchase['idProvider']; ?>">
+                              <?php echo $pro_purchase['providerName']; ?>
+                            </option>
+                            <?php 
+                                  }
+                                } catch (Exception $e) {
+                                    echo "Error: " . $e->getMessage();
+                                }
+                              ?>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row">
+
+                    <div class="form-group col-lg-3">
+                      <label for="noBill">No. de factura</label>
+                      <input type="text" class="form-control" id="noBill" name="noBill" placeholder="Escriba un número de factura">
+                    </div>
+
+                    <div class="form-group col-lg-2">
+                      <label for="serie">Serie</label>
+                      <input type="text" class="form-control" id="serie" name="serie" placeholder="Escriba una serie">
+                    </div>
+
+                    <div class="form-group col-lg-4">
+                      <label for="noDocument">No. de documento</label>
+                      <input type="text" class="form-control" id="noDocument" name="noDocument" placeholder="Escriba un número de documento">
+                    </div>
+
+                    <div class="form-group col-lg-3">
+                      <label for="addProducts">Productos</label>
+                      <button type="button" class="btn btn-warning">
+                        <i class="fa fa-search" aria-hidden="true"></i> Buscar Productos</button>
+                    </div>
+                  </div>
+                  <div class="box-footer">
+                    <div class="row">
+
+                      <div class="form-group col-lg-3 pull-right">
+                        <div class="input-group">
+                          <span class="input-group-addon">
+                            <span class="text-danger text-uppercase">*</span>
+                            <label for="totalPurchase" class="control-label">Total: Q </label>
+                          </span>
+                          <input type="number" id="totalPurchase" name="totalPurchase" placeholder="0.00" min="0.00" step="0.01" class="form-control">
+
+                        </div>
+                      </div>
+
+                    </div>
+
+                    <div class="row">
+                      <div class="form-group col-lg-5 pull-right">
+                        <input type="hidden" name="producto" value="nuevo">
+                        <span class="text-warning">Debe llenar los campos obligatorios *</span>
+                          <button type="submit" class="btn btn-primary pull-right" id="crear-producto">
+                            <i class="fa fa-floppy-o" aria-hidden="true"></i> Guardar</button>
+                      </div>
+                    </div>
+
+                  </div>
+              </form>
+              <!-- /.form-group -->
               </div>
-              <!-- /.box-body -->
-              <div class="box-footer">
-                <input type="hidden" name="producto" value="nuevo">
-                <button type="submit" class="btn btn-primary" id="crear-producto">
-                  <i class="fa fa-floppy-o" aria-hidden="true"></i> Guardar</button>
-                <span class="text-warning"> Debe llenar los campos obligatorios *</span>
-              </div>
+              <!-- /.col -->
             </div>
-            </form>
-            <!-- /.form-group -->
+            <!-- /.row -->
           </div>
-          <!-- /.col -->
-        </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.box-body -->
-      <!-- /.box -->
+          <!-- /.box-body -->
+          <!-- /.box -->
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    </div>
+    <!-- /.content-wrapper -->
 
-  <?php
+    <?php
 include_once 'templates/footer.php';
 ?>
