c0e570a9205e7182fc27e92d754261f9e36f142d
LuisPedroChaves <luispedrochaves@hotmail.com>
2018-09-05 12:29:35 -0600

    Swal de pregunta al imprimir

--- js/ajax/sale-ajax.js
+++ js/ajax/sale-ajax.js
@@ -475,6 +475,7 @@ $(document).ready(function () {
 function recargarPagina() {
     location.reload();
 }
+
 function generarFactura() {
     var serie = $("#serieS").val();
     var last = parseInt($("#noBillS").val());
@@ -502,82 +503,101 @@ function generarFactura() {
 }
 
 function imprimir(tipo, idSale) {
-    changeReportL(tipo + '.php?idSale=' + idSale);
+        if (tipo == 'factura') {
+            swal({
+                title: '¿Estás Seguro?',
+                text: "Generar una impresión altera al correlativo actual",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, Generar!',
+                cancelButtonText: 'Cancelar'
+            }).then(() => {
+            $.ajax({
+                type: "GET",
+                url: 'BLL/correlativeFactura.php',
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    $.each(data, function (key, registro) {
+                        var serie = registro.serie;
+                        var last = parseInt(registro.last) + parseInt(1);
 
-    if (tipo == 'factura') {
-        $.ajax({
-            type: "GET",
-            url: 'BLL/correlativeFactura.php',
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                $.each(data, function (key, registro) {
-                    var serie = registro.serie;
-                    var last = parseInt(registro.last) + parseInt(1);
-
-                    updateCorrelativo('factura', serie, last);
-                    $.ajax({
-                        type: "POST",
-                        data: {
-                            'venta': 'editarCorrelativo',
-                            'idSale': idSale,
-                            'serie': serie,
-                            'last': last
-                        },
-                        url: 'BLL/sale.php',
-                        datatype: 'json',
-                        success: function (data) {
-                            console.log(data);
-                            var resultado = JSON.parse(data);
-                            if (resultado.respuesta == 'exito') {                                
-                            } else if (resultado.respuesta == 'vacio') {
-                            } else if (resultado.respuesta == 'error') {
+                        updateCorrelativo('factura', serie, last);
+                        $.ajax({
+                            type: "POST",
+                            data: {
+                                'venta': 'editarCorrelativo',
+                                'idSale': idSale,
+                                'serie': serie,
+                                'last': last
+                            },
+                            url: 'BLL/sale.php',
+                            datatype: 'json',
+                            success: function (data) {
+                                console.log(data);
+                                var resultado = JSON.parse(data);
+                                if (resultado.respuesta == 'exito') {} else if (resultado.respuesta == 'vacio') {} else if (resultado.respuesta == 'error') {}
                             }
-                        }
-                    })
-                });
-            },
-            error: function (data) {
-                alert('error');
-            }
+                        })
+                    });
+                },
+                error: function (data) {
+                    alert('error');
+                }
+            });
+            changeReportL(tipo + '.php?idSale=' + idSale);
+            $('#modal-printS').modal('show');
         });
-    } else if (tipo == 'guia') {
-        $.ajax({
-            type: "GET",
-            url: 'BLL/correlativeGuia.php',
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                $.each(data, function (key, registro) {
-                    var last = parseInt(registro.last) + parseInt(1);
-                    updateCorrelativo('envio', 'A', last);
-                    $.ajax({
-                        type: "POST",
-                        data: {
-                            'venta': 'editarShipment',
-                            'idSale': idSale,
-                            'last': last
-                        },
-                        url: 'BLL/sale.php',
-                        datatype: 'json',
-                        success: function (data) {
-                            console.log(data);
-                            var resultado = JSON.parse(data);
-                            if (resultado.respuesta == 'exito') {                                
-                            } else if (resultado.respuesta == 'vacio') {
-                            } else if (resultado.respuesta == 'error') {
+        } else if (tipo == 'guia') {
+            swal({
+                title: '¿Estás Seguro?',
+                text: "Generar una impresión altera al correlativo actual",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, Generar!',
+                cancelButtonText: 'Cancelar'
+            }).then(() => {
+            $.ajax({
+                type: "GET",
+                url: 'BLL/correlativeGuia.php',
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    $.each(data, function (key, registro) {
+                        var last = parseInt(registro.last) + parseInt(1);
+                        updateCorrelativo('envio', 'A', last);
+                        $.ajax({
+                            type: "POST",
+                            data: {
+                                'venta': 'editarShipment',
+                                'idSale': idSale,
+                                'last': last
+                            },
+                            url: 'BLL/sale.php',
+                            datatype: 'json',
+                            success: function (data) {
+                                console.log(data);
+                                var resultado = JSON.parse(data);
+                                if (resultado.respuesta == 'exito') {} else if (resultado.respuesta == 'vacio') {} else if (resultado.respuesta == 'error') {}
                             }
-                        }
-                    })
-                });
-            },
-            error: function (data) {
-                alert('error');
-            }
+                        })
+                    });
+                },
+                error: function (data) {
+                    alert('error');
+                }
+            });
+            changeReportL(tipo + '.php?idSale=' + idSale);
+            $('#modal-printS').modal('show');
         });
-    }
-
-    $('#modal-printS').modal('show');
+        }else{
+            changeReportL(tipo + '.php?idSale=' + idSale);
+            $('#modal-printS').modal('show');
+        }        
 }
 
 function convertDate(dateString) {
