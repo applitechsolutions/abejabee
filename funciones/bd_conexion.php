0154b93e03016f4c86b29917ec253f2603c386bf
LnkColumbus <marlonveliz.nero19@gmail.com>
2018-10-10 20:10:22 -0600

    localhost

--- funciones/bd_conexion.php
+++ funciones/bd_conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-/*$conn = new mysqli('us-cdbr-azure-central-a.cloudapp.net', 'b4ca207be675e3', '45d554a4', 'schlenker');
+/*$conn = new mysqli('localhost', 'root', '', 'localdb');
 
 if ($conn->connect_error) {
     echo $error -> $conn->connect_error;
