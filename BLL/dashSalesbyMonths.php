001c7da8402bb97a9bcc6eee5f370240b7179afb
luispedrochaves@hotmail.com <luispedrochaves@hotmail.com>
2018-11-26 18:46:35 -0600

    Dashboard 1.4

--- BLL/dashSalesbyMonths.php
+++ BLL/dashSalesbyMonths.php
@@ -3,7 +3,7 @@
     header("Content-Type: application/json; charset=UTF-8");
 
     $conn->query("SET lc_time_names = 'es_ES'");
-    $result = $conn->query("SELECT MONTHNAME(dateStart) as month, COUNT(idSale) AS ventasT FROM sale WHERE cancel = 0 AND state = 0 AND YEAR(dateStart) =  YEAR(CURDATE()) GROUP BY MONTH(dateStart) ORDER BY MONTH(dateStart)");
+    $result = $conn->query("SELECT MONTHNAME(dateStart) as month, COUNT(idSale) AS ventasT FROM sale WHERE state = 0 AND YEAR(dateStart) =  YEAR(CURDATE()) GROUP BY MONTH(dateStart) ORDER BY MONTH(dateStart)");
     $outp = array();
     $outp = $result->fetch_all(MYSQLI_ASSOC);
 
