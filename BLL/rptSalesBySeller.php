eb0f7b5d9ded4217a81d06dd34288175ab5a88ce
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-05-06 20:09:37 -0600

    Nueva Query de comisiones

--- BLL/rptSalesBySeller.php
+++ BLL/rptSalesBySeller.php
@@ -9,26 +9,10 @@
     $fi = date('Y-m-d', strtotime($fechainicio));
     $ff = date('Y-m-d', strtotime($fechafinal));
 
-    $result = $conn->query("SELECT S.*, 
-    (SELECT date FROM balance WHERE _idSale = S.idSale ORDER BY idBalance DESC LIMIT 1) as fechapago,
-    quantity, priceS, discount,
-    (SELECT productName FROM product WHERE idProduct = D._idProduct) as nombre,
-    (SELECT productCode FROM product WHERE idProduct = D._idProduct) as codigo,
-    (SELECT s30 FROM commission WHERE _idSeller = $idSeller) as s30,
-    (SELECT s60 FROM commission WHERE _idSeller = $idSeller) as s60,
-    (SELECT s90 FROM commission WHERE _idSeller = $idSeller) as s90,
-    (SELECT o30 FROM commission WHERE _idSeller = $idSeller) as o30,
-    (SELECT o60 FROM commission WHERE _idSeller = $idSeller) as o60,
-    (SELECT sd30 FROM commission WHERE _idSeller = $idSeller) as sd30,
-    (SELECT sd60 FROM commission WHERE _idSeller = $idSeller) as sd60,
-    (SELECT sd90 FROM commission WHERE _idSeller = $idSeller) as sd90,
-    (SELECT od30 FROM commission WHERE _idSeller = $idSeller) as od30,
-    (SELECT od60 FROM commission WHERE _idSeller = $idSeller) as od60,
-    (SELECT makeName FROM make WHERE idMake = (SELECT _idMake FROM product WHERE idProduct = D._idProduct)) as marca
-     FROM sale S INNER JOIN detailS D ON S.idSale = D._idSale
-     WHERE S.cancel = 1 AND S._idSeller = $idSeller AND (SELECT date FROM balance WHERE _idSale = S.idSale ORDER BY idBalance DESC LIMIT 1) BETWEEN '$fi' AND '$ff' ORDER BY S.dateStart ASC");
+    $result = $conn->query("SELECT S.dateStart, S.noDeliver, B.noDocument, B.date, B.amount, B.noReceipt, B.totalS, B.totalO
+    FROM sale S INNER JOIN balance B ON S.idSale = B._idSale WHERE S.state = 0 AND B.balpay = 1 AND B.state = 0 AND 
+    B.date BETWEEN $fi AND $ff AND S._idSeller = $idSeller ORDER BY B.date ASC;");
     $outp = array();
     $outp = $result->fetch_all(MYSQLI_ASSOC);
 
     echo json_encode($outp);
-?>
