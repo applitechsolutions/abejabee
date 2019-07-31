dccc50f7cd6a66d89786f93298ee1c02d5847a0a
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-07-31 12:26:25 -0600

    Ahora si

--- BLL/rptstockByProductV.php
+++ BLL/rptstockByProductV.php
@@ -8,7 +8,7 @@ $fechainicio = strtr($_POST['dateSrpt6'], '/', '-');
 $fi = date('Y-m-d', strtotime($fechainicio));
 
 $result = $conn->query("SELECT S.dateStart, S.noDeliver, S.note, (D.priceS - D.discount) as price, D.quantity, 
-(select sum(stock) from storage where _idProduct = D.idProduct) as stock
+(select sum(stock) from storage where _idProduct = D._idProduct) as stock
 FROM `details` D INNER JOIN sale S ON D._idSale = S.idSale WHERE D._idProduct = $product AND S.dateStart >= '$fi' AND S.state = 0");
 $outp = array();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
