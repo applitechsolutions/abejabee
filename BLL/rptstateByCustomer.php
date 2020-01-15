819667188893c87de1ab063ba904788b834e4534
luispedrochaves@hotmail.com <luispedrochaves@hotmail.com>
2020-01-15 12:05:57 -0600

    Correccion de saldo por remision

--- BLL/rptstateByCustomer.php
+++ BLL/rptstateByCustomer.php
@@ -8,7 +8,7 @@ $result = $conn->query("SELECT idCustomer, customerCode, customerName, customerT
 (SELECT name FROM deparment where idDeparment = C._idDeparment) as deparment,
 (SELECT routeName FROM route WHERE idRoute = C._idRoute) as route, S.noDeliver, S.dateStart, S.advance, S.dateEnd, S.type, S.cancel, S.totalSale,
 (SELECT concat(sellerFirstName, ' ', sellerLastName) FROM seller where idSeller = S._idSeller) as seller,
-(SELECT balance FROM balance WHERE _idSale = idSale ORDER BY idBalance DESC LIMIT 1) AS saldo,
+(SELECT balance FROM balance WHERE _idSale = idSale AND state != 2 ORDER BY idBalance DESC LIMIT 1) AS saldo,
 (SELECT SUM(amount) FROM balance WHERE _idSale = idSale AND cheque = 1 AND state = 1) AS abono
 FROM customer C INNER JOIN sale S ON C.idCustomer = S._idCustomer
 WHERE C.idCustomer = $idCustomer AND S.state = 0 ORDER BY S.dateStart DESC");
